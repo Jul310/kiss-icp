f648d7e230ffeb2af32ecfb5dec2949123d5aaf7
Julian Dauth <Julian.Dauth@tum.de>
2023-10-01 18:47:06 +0200

    Implemented compare plot function

--- tum_eval/tools/tum_plots.py
+++ tum_eval/tools/tum_plots.py
@@ -127,6 +127,40 @@ def plot_compare(traj: TrajectoryPair,
     return metric.get_result()
 
 
+def compare_plot_multiple(trajecotries, 
+                          names=None,
+                          plot_mode='xyz',
+                          **kwargs):
+    plt.close('all')
+    if names is not None:
+        assert len(names) == len(trajecotries), "Number of given names must be equal to the number of trajecotries"
+    else:
+        names = [i for i in range(1, len(trajecotries))]
+
+    _, axarr = plt.subplots(3, figsize=get_figsize(wf=1,hf=1))
+    
+    plot_fn = None
+    if plot_mode == 'xyz':
+        plot_fn = plot.traj_xyz
+    elif plot_mode == 'rpy':
+        plot_fn = plot.traj_rpy
+    else:
+        raise RuntimeError(f"Unsupported Plot mode: plot_mode={plot_mode}. Must be one of ['xyz', 'rpy']")
+    
+    prop_cycle = plt.rcParams['axes.prop_cycle']
+    colors = prop_cycle.by_key()['color']
+    for i, (t,n) in enumerate(zip(trajecotries, names)):
+        linestyle="-"
+        color = colors[i]
+        
+        if n.lower() == 'reference':
+            linestyle = "--"
+            color = 'gray'
+
+        plot_fn(axarr, t, linestyle, color, n)
+    plt.show()
+    
+
 def plot_rpe_errors(traj_ref, traj_est, title=None, pose_relation=metrics.PoseRelation.translation_part):
     rpe_metric = metrics.RPE(pose_relation=pose_relation)
     rpe_metric.process_data((traj_ref, traj_est))
